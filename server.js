9c5cabc29bb6c11e0afe6643bcf4993fb01dbfce
Jesse Chong <jessechong@pursuit.org>
2023-09-05 00:55:31 -0400

    Finished lab

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const port = 3005;
+const port = 3010;
 
-app.get('/callback', (req, res) => {
-    res.send('Temporary Redirect URL Page');
-});
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+app.get("/fetch", async (req, res) => {
+    try {
+        const { url } = req.query;
+
+        if (!url) {
+            return res.status(400).send("URL is missing.")
+        }
+      const authHeader = `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`;
+  
+      const authResponse = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        "grant_type=client_credentials",
+        {
+          headers: {
+            'Authorization': authHeader,
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+        }
+      );
+  
+      const accessToken = authResponse.data.access_token;
+  
+
+      const response = await axios.get(
+        url, {
+          headers: {
+            'Authorization': `Bearer ${accessToken}`
+          }
+        }
+      );
+  
+      res.json(response.data);
+    } catch (error) {
+      console.error("Error", error);
+      res.status(500).send("Error occurred");
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
